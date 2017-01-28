367668dc482b4df409bd939a29e40b47629703d2
Angelos Orfanakos <me@agorf.gr>
2017-01-28 15:19:20 +0200

    Add upsetRecordSet() function and relevant struct

--- dyndns53.go
+++ dyndns53.go
@@ -18,19 +18,27 @@ import (
 	"github.com/aws/aws-sdk-go/service/route53"
 )
 
+type recordSet struct {
+	Name         string
+	Value        string // ip
+	Type         string
+	TTL          int64
+	HostedZoneId string
+}
+
 const progName = "dyndns53"
 
 func main() {
 	log.SetPrefix(progName + ": ")
 	log.SetFlags(0)
 
-	var recordSetName, recordSetType, hostedZoneId string
-	var recordSetTTL int64
+	var err error
+	var recSet recordSet
 
-	flag.StringVar(&recordSetName, "name", "", `record set name; must end with "."`)
-	flag.StringVar(&recordSetType, "type", "A", `record set type; "A" or "AAAA"`)
-	flag.Int64Var(&recordSetTTL, "ttl", 300, "TTL (time to live) in seconds")
-	flag.StringVar(&hostedZoneId, "zone", "", "hosted zone id")
+	flag.StringVar(&recSet.Name, "name", "", `record set name; must end with "."`)
+	flag.StringVar(&recSet.Type, "type", "A", `record set type; "A" or "AAAA"`)
+	flag.Int64Var(&recSet.TTL, "ttl", 300, "TTL (time to live) in seconds")
+	flag.StringVar(&recSet.HostedZoneId, "zone", "", "hosted zone id")
 
 	if len(os.Args) == 1 {
 		flag.Usage()
@@ -39,70 +47,36 @@ func main() {
 
 	flag.Parse()
 
-	if recordSetName == "" {
+	if recSet.Name == "" {
 		log.Fatal("missing record set name")
 	}
-	if !strings.HasSuffix(recordSetName, ".") {
+	if !strings.HasSuffix(recSet.Name, ".") {
 		log.Fatal(`record set name must end with a "."`)
 	}
-	if recordSetType == "" {
+	if recSet.Type == "" {
 		log.Fatal("missing record set type")
 	}
-	if recordSetType != "A" && recordSetType != "AAAA" {
-		log.Fatalf("invalid record set type: %s", recordSetType)
+	if recSet.Type != "A" && recSet.Type != "AAAA" {
+		log.Fatalf("invalid record set type: %s", recSet.Type)
 	}
-	if recordSetTTL < 1 {
-		log.Fatalf("invalid record set TTL: %d", recordSetTTL)
+	if recSet.TTL < 1 {
+		log.Fatalf("invalid record set TTL: %d", recSet.TTL)
 	}
-	if hostedZoneId == "" {
+	if recSet.HostedZoneId == "" {
 		log.Fatal("missing hosted zone id")
 	}
 
-	currentIP, err := getCurrentIP()
+	recSet.Value, err = getCurrentIP()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	domain := strings.TrimSuffix(recordSetName, ".")
-	if domainResolvesToIP(domain, currentIP) {
-		log.Fatalf("%s already resolves to %s; nothing to do", domain, currentIP)
+	domain := strings.TrimSuffix(recSet.Name, ".")
+	if domainResolvesToIP(domain, recSet.Value) {
+		log.Fatalf("%s already resolves to %s; nothing to do", domain, recSet.Value)
 	}
 
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	credentialsPath := path.Join(usr.HomeDir, ".aws", "credentials")
-	credentials := credentials.NewSharedCredentials(credentialsPath, progName)
-
-	sess, err := session.NewSession()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	svc := route53.New(sess, &aws.Config{Credentials: credentials})
-
-	params := &route53.ChangeResourceRecordSetsInput{
-		ChangeBatch: &route53.ChangeBatch{
-			Changes: []*route53.Change{
-				{
-					Action: aws.String("UPSERT"),
-					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String(recordSetName),
-						Type: aws.String(recordSetType),
-						TTL:  aws.Int64(recordSetTTL),
-						ResourceRecords: []*route53.ResourceRecord{
-							{
-								Value: aws.String(currentIP),
-							},
-						},
-					},
-				},
-			},
-		},
-		HostedZoneId: aws.String(hostedZoneId),
-	}
-	resp, err := svc.ChangeResourceRecordSets(params)
+	resp, err := upsertRecordSet(&recSet)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -130,4 +104,47 @@ func domainResolvesToIP(domain, checkIP string) bool {
 		}
 	}
 	return false
+}
+
+func upsertRecordSet(recSet *recordSet) (*route53.ChangeResourceRecordSetsOutput, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	credentialsPath := path.Join(usr.HomeDir, ".aws", "credentials")
+	credentials := credentials.NewSharedCredentials(credentialsPath, progName)
+
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+
+	svc := route53.New(sess, &aws.Config{Credentials: credentials})
+
+	params := &route53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &route53.ChangeBatch{
+			Changes: []*route53.Change{
+				{
+					Action: aws.String("UPSERT"),
+					ResourceRecordSet: &route53.ResourceRecordSet{
+						Name: aws.String(recSet.Name),
+						Type: aws.String(recSet.Type),
+						TTL:  aws.Int64(recSet.TTL),
+						ResourceRecords: []*route53.ResourceRecord{
+							{
+								Value: aws.String(recSet.Value),
+							},
+						},
+					},
+				},
+			},
+		},
+		HostedZoneId: aws.String(recSet.HostedZoneId),
+	}
+	resp, err := svc.ChangeResourceRecordSets(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
